eac3b4a14b0fae7342827d29f6ce6b42f29eb2bd
Rangga Dikarinata <dikarinata@gmail.com>
2022-12-29 12:45:55 +0700

    fix route

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -16,15 +16,10 @@ const RESOURCES = {
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "manifest.json": "c546a7f67e6b31741d8ee7df1c0d0273",
 "version.json": "795bc83965e1fe371d1a7927e71c555a",
-"index.html": "fa625d3033afdff8865a9fb1920a9b22",
-"/": "fa625d3033afdff8865a9fb1920a9b22",
-"flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
+"index.html": "f088ba127ce46df075172503a4e6f606",
+"/": "f088ba127ce46df075172503a4e6f606",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"main.dart.js": "456f5b024de7cf181f28b7b42cd463fb",
-"canvaskit/canvaskit.js": "2bc454a691c631b07a9307ac4ca47797",
-"canvaskit/profiling/canvaskit.js": "38164e5a72bdad0faa4ce740c9b8e564",
-"canvaskit/profiling/canvaskit.wasm": "95a45378b69e77af5ed2bc72b2209b94",
-"canvaskit/canvaskit.wasm": "bf50631470eb967688cca13ee181af62"
+"main.dart.js": "cb4fa65916b7681f30dc0cad4ec9b87c"
 };
 
 // The application shell files that are downloaded before a service worker can
